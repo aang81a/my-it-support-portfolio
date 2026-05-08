ff23a4e24978a2604668a5adcd965aeee59431c7
AF <155856773+aang81a@users.noreply.github.com>
2026-05-08 10:27:37 +0200

    Update ClientDiagnostic.ps1

--- PROJECTS/12-powershell-automation-lab/ClientDiagnostic.ps1
+++ PROJECTS/12-powershell-automation-lab/ClientDiagnostic.ps1
@@ -6,14 +6,16 @@
 Write-Host "Gathering System Information... Please wait." -ForegroundColor Cyan
 
 $Results = [PSCustomObject]@{
+    "Date/Time"      = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     "Computer Name" = $env:COMPUTERNAME
     "Current User"  = $env:USERNAME
     "IP Address"    = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -notlike "*Loopback*" }).IPAddress[0]
     "Serial Number" = (Get-CimInstance Win32_Bios).SerialNumber
     "Windows Ver"   = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ProductName
     "Last Reboot"   = (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
+    "Antivirus"      = (Get-CimInstance -Namespace root/SecurityCenter2 -ClassName AntivirusProduct).displayName
 }
-
+    
 # Output to console
 $Results | Out-String | Write-Host -ForegroundColor Green
 
